ad2f8e0fa12d0b4b0ba600106cd2af55081fb9c1
benoit moret <benoit.moret@gmail.com>
2025-05-23 11:04:46 +0200

    cre

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,4 @@ class Article < ApplicationRecord
   has_one_attached :photo
   # mon_article.photo
 
-  def photo_url
-    if photo.attached?
-      cl_image_path article.photo.key, height: 300, crop: :fill
-    else
-      ""
-    end
-  end
 end
